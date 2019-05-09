f7b6c4bda18eb7b5d848cfabd9f00c33cc259912
wajd <wajdjtohme@gmail.com>
2019-05-09 10:42:43 +0200

    replaced faulty functions

--- app/src/main/java/com/example/readsensors/TopicActivity.java
+++ app/src/main/java/com/example/readsensors/TopicActivity.java
@@ -34,7 +34,7 @@ public class TopicActivity extends AppCompatActivity {
     public void publish(View v){
         Intent intent = new Intent(this, PublishActivity.class);
 //        Button publish = (Button) findViewById(R.id.button3);
-        intent.putExtra("topic-string", topic.makeURI());
+        intent.putExtra("topic-string", topic.toString());
         startActivity(intent);
     }
 
@@ -58,7 +58,7 @@ public class TopicActivity extends AppCompatActivity {
 
         CoAP.ResponseCode response = PubSub.remove(address, 5683, topic);
 
-        Toast toast = Toast.makeText(TopicActivity.this, topic.getPath() , Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(TopicActivity.this, topic.getPathAsString() , Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
 
